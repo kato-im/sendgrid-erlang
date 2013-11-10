54a9505f2629a61110ad953e051e88676bd990d6
Andrei Soroker <soroker@gmail.com>
2013-11-10 06:56:30 -0800

    Handle errors.

--- src/esendgrid.erl
+++ src/esendgrid.erl
@@ -13,41 +13,27 @@ parse_email(String) ->
 
 send_email(Json) when is_binary(Json) ->
     Jterm = jiffy:decode(Json),
-
     Subject = ej:get({"Subject"}, Jterm),
     Text = ej:get({"TextBody"}, Jterm),
     To = ej:get({"To"}, Jterm),
     From = ej:get({"From"}, Jterm),
-
     Params0 = [
         {<<"subject">>, Subject},
         {<<"text">>, Text}
     ],
-
     Params1 = case parse_email(To) of
         {ToName, ToEmail} ->
-            Params0 ++ [
-                {<<"to">>, ToEmail},
-                {<<"toname">>, ToName}
-            ];
+            Params0 ++ [ {<<"to">>, ToEmail}, {<<"toname">>, ToName} ];
         ToEmail ->
-            Params0 ++ [
-                {<<"to">>, ToEmail}
-            ]
+            Params0 ++ [ {<<"to">>, ToEmail} ]
     end,
-
     Params2 = case parse_email(From) of
         {FromName, FromEmail} ->
-            Params1 ++ [
-                {<<"from">>, FromEmail},
-                {<<"fromname">>, FromName}
-            ];
+            Params1 ++ [ {<<"from">>, FromEmail}, {<<"fromname">>, FromName} ];
         FromEmail ->
-            Params1 ++ [
-                {<<"from">>, FromEmail}
-            ]
+            Params1 ++ [ {<<"from">>, FromEmail} ]
     end,
-    send_email(Params2);
+    handle_response(send_email(Params2));
 
 send_email(Params) when is_list(Params) ->
     {ok, ApiUser} = application:get_env(esendgrid, sendgrid_api_user),
@@ -69,6 +55,23 @@ send_email(Params) when is_list(Params) ->
         5000
     ),
     Res.
+
+handle_response({ok, {{200, _}, _, JsonResponse}}) ->
+    {ok, jiffy:decode(JsonResponse)};
+
+handle_response({ok, {{400, "Bad Request"}, _, JsonResponse}}) ->
+    Jterm = jiffy:decode(JsonResponse),
+    case ej:get({<<"message">>, <<"error">>, <<"errors">>, 0}, Jterm) of
+        <<"Invalid from email address", _/binary>> ->
+            {error, illegal_from_address};
+        <<"Invalid to email address", _/binary>> ->
+            {error, illegal_to_address};
+        _ ->
+            {error, Jterm}
+    end;
+
+handle_response({error, timeout}) ->
+    {error, timeout}.
 
 form_urlencode(Proplist) ->
     form_urlencode(Proplist, []).
